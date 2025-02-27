43c3a1049f8e37897196f8005c80de319b4c1cb1
filip <filip.szala96@gmail.com>
2025-02-27 08:38:14 +0100

    Initial frontend setup

---
+++ fullstack-frontend/dev/buynowdotcom/src/component/hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='Hero'>
+        <div className='hero-content'>
+            <h1>
+                Welcome to <span className='text-primary'> mediamate</span>.com
+            </h1>
+            <div className = "home-button-conatiner">
+                <a href="#" className="home-shop-button link">
+
+                </a>
+                <button className="deals-button">Today's Deal</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
